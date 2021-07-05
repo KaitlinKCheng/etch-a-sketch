9973e9b4db25f43fe1a5fd2f23db743e9b2d7a97
KaitlinKCheng <80509426+KaitlinKCheng@users.noreply.github.com>
2021-07-04 23:54:10 -0500

    Add setup() & createGrid()

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+/* Page Elements */
+const modeBtns = document.querySelectorAll('.mode-btn');
+const blackMode = document.getElementById('black-mode');
+const greyscaleMode = document.getElementById('greyscale-mode');
+const rainbowMode = document.getElementById('rainbow-mode');
+const sizeBtn = document.getElementById('size-btn');
+const grid = document.getElementById('grid');
+
+/* Grid Constants */
+const gridDim = 500; // px
+const maxSquares = 100;
+
+setup();
+
+/**
+ * Sets up the default etch-a-sketch grid.
+ */
+function setup() {
+    grid.style.width = `${gridDim}px`;
+    grid.style.height = `${gridDim}px`;
+    createGrid(16);
+}
+
+/**
+ * Creates a new squares x squares grid.
+ *
+ * @param {number} squares - The number of squares per side.
+ */
+function createGrid(squares) {
+    let squareDim = gridDim / squares; // px
+
+    // Clear current grid
+    let oldChildren = document.getElementById('grid').childNodes;
+    oldChildren.forEach((child) => {
+        grid.removeChild(child);
+    });
+
+    // Define space for each square
+    grid.style.grid = `repeat(${squares}, ${squareDim}px) / repeat(${squares}, ${squareDim}px)`;
+
+    for (let i = 0; i < squares * squares; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('square');
+        grid.appendChild(gridSquare);
+    }
+}
